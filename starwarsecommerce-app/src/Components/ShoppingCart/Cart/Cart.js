c5cdaa6f2085bd0523dd8c762d9e79b813b027aa
VishLucasHenrique <lucasproenca_11@hotmail.com>
2023-05-07 11:41:40 -0300

    [configuração] criei os componentes principais do projeto

---
+++ starwarsecommerce-app/src/Components/ShoppingCart/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Items from "../Items/Items";
+
+const Cart = () => {
+    return(
+        <div>
+            <Items/>
+        </div>
+    )
+}
+
+export default Cart
